ff996880d91edbff1458aa346b4f509fb527b5c7
Georgi <g.velchev12@gmail.com>
2020-04-10 15:21:32 +0300

    added recipes and shopping list components

--- Angular - The Complete Guide/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Angular - The Complete Guide/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,16 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { Recipe } from "../recipe.model";
 @Component({
-  selector: 'app-recipe-list',
-  templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  selector: "app-recipe-list",
+  templateUrl: "./recipe-list.component.html",
+  styleUrls: ["./recipe-list.component.css"]
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
-  
-  constructor() { }
+  recipes: Recipe[] = [
+    new Recipe(
+      "A Test Recipe",
+      "Ti is a simply test",
+      "https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2018/12/Shakshuka-19.jpg"
+    )
+  ];
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
